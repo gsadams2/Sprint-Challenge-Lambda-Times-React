4c98e875ac192f260f154f129d99a4aa2b379be9
George Adams <gadams@Georges-Air.local>
2019-06-07 11:32:04 -0500

    finished most of MVP but website not responding correctly, trying to fix bugs now

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React, { Component } from "react";
+import Card from "./Card";
+import PropTypes from "prop-types";
 
 const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+
+      {props.cards.map(card => {
+        return <Card card={card} key={Math.floor(Math.random() * 10 + 1)} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
+
+Cards.propTypes = {
+  card: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string.isRequired))
+};
 
 export default Cards;
